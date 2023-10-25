0e658a0f736ec8f6d216e5e2fe8a8564769504dd
zooorul <ehdnjs0218@naver.com>
2023-10-25 10:06:56 +0900

    파일 연습 완료

--- DemoFormat.py
+++ DemoFormat.py
@@ -27,12 +27,25 @@ print("{0:.2f}".format(4/3))
 f = open("demo.txt","wt", encoding = "utf-8") 
 #encoding은 지정 안해도 됨. 다만 영문, 독일  윈도우 사용하는 경우가 있어서 
 #utf-8이라고 습관적으로 지정해주기(utf-8 = uni-code라는 뜻)
-f.write("첫번째\n두번째\n세번째\n")
+f.write("첫번째\n두번째\n세번째\n") 
 f.close()
 
 #파일 읽기
 f = open("demo.txt", "rt", encoding = "utf-8")
 result=f.read()
 print(result)
+
+#처음으로 리셋
+f.seek(0)
+print(f.readline(),end="")
+print(f.readline(),end="")
+
+f.seek(0)
+lst = f.readlines()
+for item in lst:
+    print(item,end="")
+#print 함수는 끝에 \n이 고정적으로 들어가 있음.
+#따라서 end=""을 추가해줘서 \n이 고정 값으로 들어가지 않도록 함.
+    
 f.close() # close 안하면 다른 여러 사용자가 사용할 때 문제가 생길 수 있음.
 
