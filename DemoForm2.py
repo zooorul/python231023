2773a387ff85a3ef8168509bd2e2fcbe7280e1a1
zooorul <ehdnjs0218@naver.com>
2023-10-27 11:41:02 +0900

    Pandas 연습 1번

--- DemoForm2.py
+++ DemoForm2.py
@@ -3,6 +3,10 @@ import sys
 import typing 
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
+#웹서버에 요청
+import requests
+#크롤링
+from bs4 import BeautifulSoup
 
 #디자인 파일 로딩(파일명 변경)
 form_class = uic.loadUiType("DemoForm2.ui")[0]
@@ -16,6 +20,26 @@ class DemoForm(QMainWindow, form_class):
     #슬롯 메서드
     def firstClick(self) :
         self.label.setText("첫번째 버튼")
+        url = "https://www.daangn.com/fleamarket/"
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text,"html.parser")
+
+        #<div class속성 딕셔너리>
+        f =open("c:\\work\\dangn.txt","wt", encoding="utf-8")
+        posts = soup.find_all("div", attrs={"class":"card-desc"})
+        for post in posts:
+            title = post.find("h2", attrs={"class":"card-title"})
+            price = post.find("div", attrs={"class":"card-price"})
+            addr = post.find("div", attrs={"class":"card-region-name"})
+            #메서드체인(함수체인) 줄바꿈 없애기
+            title = title.text.strip().replace("\n","")
+            price = price.text.strip().replace("\n","")
+            addr = addr.text.strip().replace("\n","")
+            print("{0},{1},{2}".format(title, price, addr))
+            #f를 붙이고 변수명 넘기기
+            f.write(f"{title},{price},{addr}\n") # 쓰고 줄을 바꾸도록 해야 보기 편하게 출력됨.
+        f.close()
+        self.label.setText("당근 크롤링 완료~~")
     def secondClick(self) :
         self.label.setText("두번째 버튼~~")
     def thirdClick(self) :
